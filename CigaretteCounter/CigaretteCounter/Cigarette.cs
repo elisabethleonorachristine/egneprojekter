6d0e1fac09ab198fd936d7596e6cd17fae012eb4
elisabethleonorachristine <elisabethleonorachristine@gmail.com>
2025-03-08 07:39:48 +0100

    first try!

---
+++ CigaretteCounter/CigaretteCounter/Cigarette.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CigaretteCounter
+{
+    public class Cigarette
+    {
+        public int TotalCigs;
+        public int CigsToday;
+
+        public Cigarette(int totalCigs, int cigsToday)
+        {
+            TotalCigs = totalCigs;
+            CigsToday = cigsToday;
+        }
+
+        public void AddCig()
+        {
+            Console.WriteLine("Enter number of cigs to add:");
+            string cigNum = Console.ReadLine();
+            int cigNumInt = Int32.Parse(cigNum);
+            if (cigNumInt > 0)
+            {
+                Cigarette newCig = new Cigarette(TotalCigs, cigNumInt);
+                TotalCigs += cigNumInt;
+                Console.WriteLine($"You now have smoked {TotalCigs} cigarettes in total.");
+            }
+            
+        }
+        
+        
+   
+    } 
+    
+    
+} 
---
+++ CigaretteCounter/CigaretteCounter/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace CigaretteCounter
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            List<int> CigAmount = new List<int>();
+
+            Cigarette cigCounter = new Cigarette(0, 0);
+
+            while (true)
+            {
+                Console.WriteLine("Choose an option:\n" +
+                                  "1. Add cigarette\n" +
+                                  "2. See total cigarettes\n" +
+                                  "3. See progression" +
+                                  "4. Exit");
+                string choice = Console.ReadLine() ?? "";
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Enter number of cigs to add:");
+                        string cigNum = Console.ReadLine();
+                        int cigNumInt = Int32.Parse(cigNum);
+                        CigAmount.Add(cigNumInt);
+                        cigCounter.TotalCigs += cigNumInt;
+                        Console.WriteLine($"You now have smoked {cigCounter.TotalCigs} cigarettes in total.");
+                        break;
+                   
+
+                }
+
+
+
+            }
+        }
+    }
+}
